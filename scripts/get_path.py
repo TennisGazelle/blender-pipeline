dd6423f31048aef54754c85ddd3629152e443ddb
Daniel Lopez <daniellopez@nevada.unr.edu>
2021-05-24 23:20:53 -0700

    fix(scripts): fixed and simplified get_path.py

--- scripts/get_path.py
+++ scripts/get_path.py
@@ -5,22 +5,12 @@ import bpy
 import yaml
 import json
 
-def get_config_for_file():
-    blend_file = bpy.data.filepath
-    for stage in config.keys():
-        if isinstance(config[stage], dict):
-            if config[stage]['blend_file'] in blend_file:
-                return config[stage], stage
-    
-    return None
+from common import init_config
 
-with open('config.yaml', 'r') as config_file:
-    config = yaml.load(config_file)#, Loader=yaml.FullLoader) # fix this
+config = init_config()
 
 scene = bpy.context.scene
-file_config, stage = get_config_for_file()
 
-print('config: ', json.dumps(config, indent=3))
 print('Initial Output Filepath: {}'.format(scene.render.filepath))
 
 if bpy.data.images:
@@ -28,8 +18,5 @@ if bpy.data.images:
         if not image.filepath.strip():
             continue
 
-        print('image: ', image.filepath)
-        if '//../imgs/resources' not in image.filepath:
-            print('updating, improper - ', image.filepath)
-        else:
-            print('            proper - ', image.filepath)
+        print('image: ', image.filepath, '---- {}'.format('OK' if '//../imgs/' in image.filepath else 'NOT OK'))
+
